54223d95aa4e2bcf4a84f5010c4b2857efe880b3
Kane Koo <kanekoo@gmail.com>
2019-08-05 16:38:33 -0400

    Done.

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,10 +23,11 @@ module OmniauthLab
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    
+
     # Commenting out this next line because it causes Chrome to cache the HTTPS protocol for the localhost
     # domain, forcing students to have to clear the cache to work
 
-    #config.force_ssl = true
+    # config.force_ssl = true
+
   end
 end
